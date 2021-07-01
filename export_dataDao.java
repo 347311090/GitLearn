459046d4e5179ee9f5cb3db8f468438102ab07c8
luanhaolun <347311090@qq.com>
2021-07-01 16:02:54 +0800

    test threeGitTest

--- export_dataDao.java
+++ export_dataDao.java
@@ -12,6 +12,7 @@ import com.school.demo.entiy.project_load;
 import com.school.demo.entiy.research_load;
 //Git is a distributed version control system.
 //		Git is free software distributed under the GPL.
+//Git has a mutable index called stage.
 @Mapper
 public interface export_dataDao {
 	public List<project_load> select_project(@Param ("rpeopleName")String Name);
